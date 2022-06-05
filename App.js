195ba42acaa642f200dec8628d8d4041f49d1940
Tony Jemba <jjembatony@gmail.com>
2022-06-06 00:03:40 +0300

    works on emulator

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React from 'react';
-import { WebView } from 'react-native-webview';
+import {WebView} from 'react-native-webview';
 import type {Node} from 'react';
 import {
   SafeAreaView,
@@ -19,17 +19,23 @@ import {
   View,
 } from 'react-native';
 
-
-
 const App: () => Node = () => {
- 
   return (
     <WebView
-    source={{ uri: 'https://online-pharmacy-project.herokuapp.com/' }}
-    style={{ marginTop: 20 }}
-  />
+      source={{uri: 'https://online-pharmacy-project.herokuapp.com'}}
+      style={{marginTop: 20}}
+      javaScriptEnabled={true}
+      originWhitelist={['*']}
+      domStorageEnabled={true}
+      scalesPageToFit={true}
+      nestedScrollEnabled={false}
+      automaticallyAdjustContentInsets={false}
+      androidHardwareAccelerationDisabled={false}
+      androidLayerType={'hardware'}
+      mixedContentMode={'always'}
+      overScrollMode={'never'}
+    />
   );
 };
-
 
 export default App;
